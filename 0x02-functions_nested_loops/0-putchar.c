7fd880d93ff5acd9673bfa8602d9aab681b40015
dennisomwanda <dennisoowiti@gmail.com>
2022-12-15 09:43:25 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
- * main - prints _putchar and a new line
- * Return: Always 0
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,8 +5,10 @@
  * _putchar - writes the character c to stdout
  * gc: the character to print
  *
- * Return:on success 1
+ * Return: on success 1.
+ * On error, -1 is returned, and errno is set approprietely.
  */
 int _putchar(char c)
-
+{
 	return (write(1, &c, 1));
+}
