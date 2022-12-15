53c4d5ec659bf887242a398ed34648db5973cc36
dennisomwanda <dennisoowiti@gmail.com>
2022-12-15 11:35:27 -0800

    putchar printed

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * gc: the character to print
+ * @c: the character to print
  *
  * Return: on success 1.
  * On error, -1 is returned, and errno is set approprietely.
